d054f0d5939883d95381d9a0022ef4438d7786b1
zhasminmirzoeva <114018963+zhasminmirzoeva@users.noreply.github.com>
2023-03-04 21:49:49 +0300

    HW6

---
+++ zhmmirzoeva_PW6/zhmmirzoeva_PW6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zhmmirzoeva_PW6
+//
+//  Created by Zhasmin Mirzoeva on 04.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
